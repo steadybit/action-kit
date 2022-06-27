1f1ae342d559906c85ec363a9db13cc12bef4dc1
Ben Blackmore <ben.blackmore@steadybit.com>
2022-06-27 18:02:33 +0200

    feat: attack list JSON schema

--- json-schema/test/httpEndpointRef.spec.ts
+++ json-schema/test/httpEndpointRef.spec.ts
@@ -2,41 +2,48 @@
 // SPDX-FileCopyrightText: 2022 Steadybit GmbH
 
 import { validate } from './util';
-import schema from '../attack-api.schema.json';
+import describingEndpointRefSchema from '../schema/describingEndpointRef.json';
+import mutatingEndpointRefSchema from '../schema/mutatingEndpointRef.json';
 
 describe('httpEndpointRef', () => {
-	it('must successfully parse HTTP endpoint references', () => {
-		expect(
-			validate(schema.$defs.httpEndpointRef, {
-				method: 'GET',
-				path: '/something',
-			}).valid
-		).toEqual(true);
-	});
+	describe.each`
+		schemaName                 | method   | schema
+		${'describingEndpointRef'} | ${'GET'} | ${describingEndpointRefSchema}
+		${'mutatingEndpointRef'}   | ${'PUT'} | ${mutatingEndpointRefSchema}
+	`('$schemaName', ({ method, schema }) => {
+		it('must successfully parse HTTP endpoint references', () => {
+			expect(
+				validate(schema, {
+					method,
+					path: '/something',
+				}).valid
+			).toEqual(true);
+		});
 
-	it('must require HTTP method', () => {
-		expect(
-			validate(schema.$defs.httpEndpointRef, {
-				path: '/something',
-			}).valid
-		).toEqual(false);
-	});
+		it('must require HTTP method', () => {
+			expect(
+				validate(schema, {
+					path: '/something',
+				}).valid
+			).toEqual(false);
+		});
 
-	it('must require specific HTTP methods', () => {
-		expect(
-			validate(schema.$defs.httpEndpointRef, {
-				method: 'HEAD',
-				path: '/something',
-			}).valid
-		).toEqual(false);
-	});
+		it('must require specific HTTP methods', () => {
+			expect(
+				validate(schema, {
+					method: 'HEAD',
+					path: '/something',
+				}).valid
+			).toEqual(false);
+		});
 
-	it('must require absolute paths', () => {
-		expect(
-			validate(schema.$defs.httpEndpointRef, {
-				method: 'GET',
-				path: 'something',
-			}).valid
-		).toEqual(false);
+		it('must require absolute paths', () => {
+			expect(
+				validate(schema, {
+					method,
+					path: 'something',
+				}).valid
+			).toEqual(false);
+		});
 	});
 });
--- json-schema/test/util.ts
+++ json-schema/test/util.ts
@@ -1,21 +1,24 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2022 Steadybit GmbH
 
+import describingEndpointRef from '../schema/describingEndpointRef.json';
+import attackListResponse from '../schema/attackListResponse.json';
+import mutatingEndpointRef from '../schema/mutatingEndpointRef.json';
 import Ajv, { ErrorObject } from 'ajv';
 
-const ajv = new Ajv({allErrors: true});
+const ajv = new Ajv({ schemas: [describingEndpointRef, attackListResponse, mutatingEndpointRef], allErrors: true });
 
 export interface ValidationResult {
-  valid: boolean;
-  errors: ErrorObject[];
+	valid: boolean;
+	errors: ErrorObject[];
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function validate(schema: any, test: any): ValidationResult {
-  const validate = ajv.compile(schema);
-  const valid = validate(test);
-  return {
-    valid,
-    errors: validate.errors ?? []
-  };
+	const validate = ajv.compile(schema);
+	const valid = validate(test);
+	return {
+		valid,
+		errors: validate.errors ?? [],
+	};
 }
