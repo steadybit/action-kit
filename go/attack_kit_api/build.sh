afeda1a4536bfc704b582da6f9c0ba279f91ed87
Ben Blackmore <ben.blackmore@steadybit.com>
2022-07-18 16:28:47 +0200

    ci: verify that there are no changes in CI

--- go/attack_kit_api/build.sh
+++ go/attack_kit_api/build.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -eo pipefail
 
-"$GOPATH/bin/oapi-codegen" -config generator-config.yml ../../openapi/spec.yml > attack_kit_api.go
+oapi-codegen -config generator-config.yml ../../openapi/spec.yml > attack_kit_api.go
 
 cat extras.go.txt >> attack_kit_api.go
