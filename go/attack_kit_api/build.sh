b00e1cf0195271b5b3e49d8c950202f2e22ebceb
Ben Blackmore <ben.blackmore@steadybit.com>
2022-07-18 16:18:14 +0200

    test: OpenAPI spec through the generated go types

--- go/attack_kit_api/build.sh
+++ go/attack_kit_api/build.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 
 "$GOPATH/bin/oapi-codegen" -config generator-config.yml ../../openapi/spec.yml > attack_kit_api.go
 
-cat extras.go >> attack_kit_api.go
+cat extras.go.txt >> attack_kit_api.go
