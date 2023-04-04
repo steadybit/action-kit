a30b45b79654ba17a1d0913eb0cca9740f36f653
Daniel Reuter <daniel@steadybit.com>
2023-04-04 15:37:00 +0200

    test: fix state_persister tests

--- go/action_kit_sdk/state_persister/state_persister_test.go
+++ go/action_kit_sdk/state_persister/state_persister_test.go
@@ -6,6 +6,7 @@ package state_persister
 import (
 	"context"
 	"github.com/google/uuid"
+	"github.com/steadybit/action-kit/go/action_kit_api/v2"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -20,9 +21,9 @@ func TestInmemoryStatePersister_basics(t *testing.T) {
 	exe1 := uuid.New()
 	exe2 := uuid.New()
 
-	err := persister.PersistState(context.Background(), &PersistedState{exe1, "action-1", &exampleState{"test", 1}})
+	err := persister.PersistState(context.Background(), &PersistedState{exe1, "action-1", action_kit_api.ActionState{"test": 1}})
 	require.NoError(t, err)
-	err = persister.PersistState(context.Background(), &PersistedState{exe2, "action-1", &exampleState{"test", 2}})
+	err = persister.PersistState(context.Background(), &PersistedState{exe2, "action-1", action_kit_api.ActionState{"test": 2}})
 	require.NoError(t, err)
 
 	states, err := persister.GetStates(context.Background())
@@ -40,7 +41,7 @@ func TestInmemoryStatePersister_basics(t *testing.T) {
 func TestInmemoryStatePersister_should_ignore_not_found(t *testing.T) {
 	persister := NewInmemoryStatePersister()
 	exe1 := uuid.New()
-	err := persister.PersistState(context.Background(), &PersistedState{exe1, "action-1", &exampleState{"test", 1}})
+	err := persister.PersistState(context.Background(), &PersistedState{exe1, "action-1", action_kit_api.ActionState{"test": 1}})
 	require.NoError(t, err)
 
 	err = persister.DeleteState(context.Background(), uuid.New())
@@ -54,14 +55,14 @@ func TestInmemoryStatePersister_should_ignore_not_found(t *testing.T) {
 func TestInmemoryStatePersister_should_update_existing_values(t *testing.T) {
 	persister := NewInmemoryStatePersister()
 	exe1 := uuid.New()
-	err := persister.PersistState(context.Background(), &PersistedState{exe1, "action-1", &exampleState{"test", 1}})
+	err := persister.PersistState(context.Background(), &PersistedState{exe1, "action-1", action_kit_api.ActionState{"test": 1}})
 	require.NoError(t, err)
 
-	err = persister.PersistState(context.Background(), &PersistedState{exe1, "action-1", &exampleState{"updated", 200}})
+	err = persister.PersistState(context.Background(), &PersistedState{exe1, "action-1", action_kit_api.ActionState{"test": 100}})
 	require.NoError(t, err)
 
 	states, err := persister.GetStates(context.Background())
 	require.NoError(t, err)
 	require.Len(t, states, 1)
-	require.Equal(t, "updated", states[0].State.(*exampleState).stringValue)
+	require.Equal(t, 100, states[0].State["test"])
 }
