73f72ccfe9994f577c2b1283c1bd2dc0742282bd
AleksaBajat <aleksabajat15@gmail.com>
2025-02-03 16:53:36 +0100

    Windows Support - Firewall creation and deletion changes - Added flushdns at the end of ModeAdd - RemoteAddress flag added only if it is not all inclusive Ipv4/Ipv6

--- go/action_kit_commons/network/blackhole_windows.go
+++ go/action_kit_commons/network/blackhole_windows.go
@@ -31,37 +31,19 @@ func (o *BlackholeOpts) FwCommands(family Family, mode Mode) ([]string, error) {
 
 		remoteAddress := ""
 
-		if net.IP.Equal(NetAnyIpv4.IP) {
+		if !net.IP.Equal(NetAnyIpv4.IP) && !net.IP.Equal(NetAnyIpv6.IP) {
 			remoteAddress = fmt.Sprintf("-RemoteAddress %s", remoteAddress)
 		}
 
 		if mode == ModeAdd {
-			cmds = append(cmds, fmt.Sprintf("New-NetFirewallRule -Name STEADYBIT_%s_%d_%d_%s -Direction Outbound -RemotePort %d-%d %s", family, portRange.From, portRange.To, net.IP, portRange.From, portRange.To, remoteAddress))
+			cmds = append(cmds, fmt.Sprintf("New-NetFirewallRule -DisplayName STEADYBIT_%s_%d_%d_%s_%s -Direction Outbound -RemotePort %d-%d -Protocol %s %s",
+				family, portRange.From, portRange.To, net.IP, IpProtoTcp, portRange.From, portRange.To, IpProtoTcp, remoteAddress))
+			cmds = append(cmds, fmt.Sprintf("New-NetFirewallRule -DisplayName STEADYBIT_%s_%d_%d_%s_%s -Direction Outbound -RemotePort %d-%d -Protocol %s %s",
+				family, portRange.From, portRange.To, net.IP, IpProtoUdp, portRange.From, portRange.To, IpProtoUdp, remoteAddress))
+			cmds = append(cmds, "ipconfig /flushdns")
 		} else if mode == ModeDelete {
-			cmds = append(cmds, fmt.Sprintf("Remove-NetFirewallRule -Name STEADYBIT_%s_%d_%d_%s", family, portRange.From, portRange.To, net.IP))
-		}
-	}
-
-	for _, nwp := range filter.Exclude {
-		net := nwp.Net
-		portRange := nwp.PortRange
-
-		if netFamily, err := getFamily(net); err != nil {
-			return nil, err
-		} else if netFamily != family {
-			continue
-		}
-
-		remoteAddress := ""
-
-		if net.IP.Equal(NetAnyIpv4.IP) {
-			remoteAddress = fmt.Sprintf("-RemoteAddress %s", remoteAddress)
-		}
-
-		if mode == ModeAdd {
-			cmds = append(cmds, fmt.Sprintf("New-NetFirewallRule -Name STEADYBIT_%s_%d_%d_%s -Direction Outbound -RemotePort %d-%d %s", family, portRange.From, portRange.To, net.IP, portRange.From, portRange.To, remoteAddress))
-		} else if mode == ModeDelete {
-			cmds = append(cmds, fmt.Sprintf("Remove-NetFirewallRule -Name STEADYBIT_%s_%d_%d_%s", family, portRange.From, portRange.To, net.IP))
+			cmds = append(cmds, fmt.Sprintf("Remove-NetFirewallRule -DisplayName STEADYBIT_%s_%d_%d_%s_%s", family, portRange.From, portRange.To, net.IP, IpProtoTcp))
+			cmds = append(cmds, fmt.Sprintf("Remove-NetFirewallRule -DisplayName STEADYBIT_%s_%d_%d_%s_%s", family, portRange.From, portRange.To, net.IP, IpProtoUdp))
 		}
 	}
 
