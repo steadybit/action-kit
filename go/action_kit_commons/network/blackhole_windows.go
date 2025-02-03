f7daa7a4486be4a6e53b424be1d0ba84fbd01472
AleksaBajat <aleksabajat15@gmail.com>
2025-02-03 17:10:28 +0100

    Windows Support - /flushdns triggers only once - FW rule action 'Block'

--- go/action_kit_commons/network/blackhole_windows.go
+++ go/action_kit_commons/network/blackhole_windows.go
@@ -36,15 +36,18 @@ func (o *BlackholeOpts) FwCommands(family Family, mode Mode) ([]string, error) {
 		}
 
 		if mode == ModeAdd {
-			cmds = append(cmds, fmt.Sprintf("New-NetFirewallRule -DisplayName STEADYBIT_%s_%d_%d_%s_%s -Direction Outbound -RemotePort %d-%d -Protocol %s %s",
+			cmds = append(cmds, fmt.Sprintf("New-NetFirewallRule -DisplayName STEADYBIT_%s_%d_%d_%s_%s -Direction Outbound -RemotePort %d-%d -Protocol %s -Action Block %s",
 				family, portRange.From, portRange.To, net.IP, IpProtoTcp, portRange.From, portRange.To, IpProtoTcp, remoteAddress))
-			cmds = append(cmds, fmt.Sprintf("New-NetFirewallRule -DisplayName STEADYBIT_%s_%d_%d_%s_%s -Direction Outbound -RemotePort %d-%d -Protocol %s %s",
+			cmds = append(cmds, fmt.Sprintf("New-NetFirewallRule -DisplayName STEADYBIT_%s_%d_%d_%s_%s -Direction Outbound -RemotePort %d-%d -Protocol %s -Action Block %s",
 				family, portRange.From, portRange.To, net.IP, IpProtoUdp, portRange.From, portRange.To, IpProtoUdp, remoteAddress))
-			cmds = append(cmds, "ipconfig /flushdns")
 		} else if mode == ModeDelete {
 			cmds = append(cmds, fmt.Sprintf("Remove-NetFirewallRule -DisplayName STEADYBIT_%s_%d_%d_%s_%s", family, portRange.From, portRange.To, net.IP, IpProtoTcp))
 			cmds = append(cmds, fmt.Sprintf("Remove-NetFirewallRule -DisplayName STEADYBIT_%s_%d_%d_%s_%s", family, portRange.From, portRange.To, net.IP, IpProtoUdp))
 		}
+	}
+
+	if mode == ModeAdd {
+		cmds = append(cmds, "ipconfig /flushdns")
 	}
 
 	return cmds, nil
