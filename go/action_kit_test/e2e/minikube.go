cc2e894f39f024df6b8b670cfb8e1cb0025ca5d0
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2025-05-06 14:07:47 +0200

    feat: add minikube kubectl

--- go/action_kit_test/e2e/minikube.go
+++ go/action_kit_test/e2e/minikube.go
@@ -149,6 +149,10 @@ func (m *Minikube) SshExec(arg ...string) *exec.Cmd {
 	return m.command(append([]string{"ssh", "--"}, arg...)...)
 }
 
+func (m *Minikube) KubectlExec(arg ...string) *exec.Cmd {
+	return m.command(append([]string{"kubectl"}, arg...)...)
+}
+
 func (m *Minikube) LoadImage(image string) error {
 	return m.command("image", "load", image).Run()
 }
